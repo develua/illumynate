95f41f7c2721cb485819294d70707ec6a783f5d1
ejayson <ethanjayson@Ethans-MacBook-Air.local>
2016-07-07 22:11:23 -0400

    updated with new api key and secret

--- ins/instagram/instagram.config.php
+++ ins/instagram/instagram.config.php
@@ -1,8 +1,8 @@
 <?php
 // Setup class
   $instagram = new Instagram(array(
-    'apiKey'      => 'c1b0ea2a268140178b70101227cc237f',
-    'apiSecret'   => '30daeaa3bc6f41da90b7ec6cf856ad48',
+    'apiKey'      => 'c64dcf187afd47a5b8095481d36627c7',
+    'apiSecret'   => 'bd64480e4c1548e287925ac232c49e84',
     'apiCallback' => 'http://illumynate.com/ins/instagram/success.php' // must point to success.php
   ));
 
