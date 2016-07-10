b965b8e6051101754e393b9406eb922738364cb8
ejayson <ethanjayson@Ethans-MacBook-Air.local>
2016-07-10 16:32:18 -0400

    credential error?

--- connect.php
+++ connect.php
@@ -4,6 +4,6 @@ $user='phpmyadmin';
 $pass='illumyn8';
 $db='socialapp';
 
-$conn=mysql_connect($host,$user,$pass) or die("Cridential error");
+$conn=mysql_connect($host,$user,$pass) or die("Cridential error?!?");
 mysql_select_db($db,$conn) or die("Database Not Selected");
 ?>
