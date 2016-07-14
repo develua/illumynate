89bf85a4b3d7ffc1d8e30c74f50e2eda5cea786e
ejayson <illumynate2016@gmail.com>
2016-07-13 23:27:36 -0400

    Update index.php

--- index.php
+++ index.php
@@ -100,7 +100,7 @@ require_once('connect.php');
 			<div class="container">
 				<div class="row">
 					<div class="col-md-6 col-md-offset-3">
-						<h1 class="text-center text-bold text-light mt-4x">Sign in?</h1>
+						<h1 class="text-center text-bold text-light mt-4x">Sign in!</h1>
 						<div class="well row pt-2x pb-3x bk-light">
 							<div class="col-md-8 col-md-offset-2">
 								<form action="" class="mt" id="login" method="post">
