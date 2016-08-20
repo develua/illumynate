ba8ecf813aeaf0ea3ac9d038c5534dc17a84c8ce
ejayson <ethanjayson@Ethans-MacBook-Air.local>
2016-08-20 11:16:47 -0400

    Updated connect.php

--- connect.php
+++ connect.php
@@ -1,9 +1,9 @@
 <?php
 $host='localhost';
-$user='winne682_social';
-$pass='S!5b?bclwRxx';
-$db='winne682_socialapp';
+$user='phpmyadmin';
+$pass='illumyn8';
+$db='socialapp';
 
-$conn=mysql_connect($host,$user,$pass) or die("Cridential error");
+$conn=mysql_connect($host,$user,$pass) or die("Cridential error?!?");
 mysql_select_db($db,$conn) or die("Database Not Selected");
 ?>
